424e35ba0e585cda2ab087e90347e51e1b9f56c9
Dev <nameofdeev@gmail.com>
2025-10-19 21:52:09 +0545

    Applies git ignore

--- ghummfir/settings.py
+++ ghummfir/settings.py
@@ -11,19 +11,19 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+SECRET_KEY = config('SECRET_KEY')
+
+DEBUG = config('DEBUG', default=False, cast=bool)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kvms-%$2xgs-73bfc$-$s!0cfn1aoyl3l)bq52-wg&q$5s62m3'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
