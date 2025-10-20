1dcf07617cc578e665a5da711ec520990e27b07a
Dev <nameofdeev@gmail.com>
2025-10-20 20:25:33 +0545

    Added models of category and place

--- guides/models.py
+++ guides/models.py
@@ -1,3 +1,56 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True)
+    slug = models.SlugField(unique=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+
+class Place(models.Model):
+    name = models.CharField(max_length=150)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='places')
+    location = models.CharField(max_length=150)
+    description = models.TextField()
+    image = models.ImageField(upload_to='places/', blank=True, null=True)
+    rating = models.DecimalField(max_digits=3, decimal_places=1, default=0.0)
+    slug = models.SlugField(unique=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+
+class Hotel(models.Model):
+    place = models.ForeignKey(Place, on_delete=models.CASCADE, related_name='hotels')
+    name = models.CharField(max_length=150)
+    address = models.CharField(max_length=200)
+    contact = models.CharField(max_length=50, blank=True)
+    price_range = models.CharField(max_length=50, blank=True)
+    image = models.ImageField(upload_to='hotels/', blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.place.name})"
+
+
+class Review(models.Model):
+    place = models.ForeignKey(Place, on_delete=models.CASCADE, related_name='reviews')
+    name = models.CharField(max_length=100)
+    rating = models.PositiveIntegerField(default=1)
+    comment = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Review for {self.place.name} by {self.name}"
