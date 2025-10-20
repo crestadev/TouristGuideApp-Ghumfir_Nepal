c3de42cfde72301b2c1bc1b7b5e0b88b7dfc18b3
Dev <nameofdeev@gmail.com>
2025-10-20 20:31:11 +0545

    Added templates regarding the place/list.html

--- ghummfir/urls.py
+++ ghummfir/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from guides import views as guide_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', guide_views.home, name='home'),
+    path('places/', include('guides.urls')),
 ]
