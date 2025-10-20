e0ff8d608964df7fe2075423470435b6bdcf7b68
Dev <nameofdeev@gmail.com>
2025-10-20 20:26:11 +0545

    Registeered the models in admin

--- guides/admin.py
+++ guides/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Category, Place, Hotel, Review
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Place)
+admin.site.register(Hotel)
+admin.site.register(Review)
