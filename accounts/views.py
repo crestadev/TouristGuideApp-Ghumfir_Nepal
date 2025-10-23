750325f4094bf491c076e50b400c6def34013569
Dev <nameofdeev@gmail.com>
2025-10-23 22:31:49 +0545

    Updated views while adding profile_views

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,30 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+
+
+
+
+@login_required
+def profile_view(request):
+    if request.method == 'POST':
+        user = request.user
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+
+        if username and email:
+            user.username = username
+            user.email = email
+            user.save()
+            messages.success(request, 'Profile updated successfully!')
+        else:
+            messages.error(request, 'All fields are required.')
+
+    return render(request, 'accounts/profile.html')
+
 
 def signup_view(request):
     if request.method == 'POST':
