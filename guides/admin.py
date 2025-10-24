99f8a3439abb0aadb6e0bf6ee22b4852296b2b6f
Dev <nameofdeev@gmail.com>
2025-10-24 15:55:54 +0545

    Updated minor changes

--- guides/admin.py
+++ guides/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Category, Place, Hotel, Review
+from .models import Category, Place, Hotel, Review, Itinerary
 
 admin.site.register(Category)
 admin.site.register(Place)
 admin.site.register(Hotel)
 admin.site.register(Review)
+admin.site.register(Itinerary)
--- guides/urls.py
+++ guides/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('places/', views.place_list, name='place_list'),
-    path('places/<slug:slug>/', views.place_detail, name='place_detail'),
-    path('places/add/<int:place_id>/', views.add_to_itinerary, name='add_to_itinerary'),
+    path('', views.place_list, name='place_list'),
+    path('<slug:slug>/', views.place_detail, name='place_detail'),
+    path('add/<int:place_id>/', views.add_to_itinerary, name='add_to_itinerary'),
 ]
