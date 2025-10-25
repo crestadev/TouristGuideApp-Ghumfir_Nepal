4b1c5705a62e8eb963914b3b001a710a63b86e23
Dev <nameofdeev@gmail.com>
2025-10-25 22:08:19 +0545

    Added favourite in accounts

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from guides.models import Place
 
-# Create your models here.
+class Favorite(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='favorites')
+    place = models.ForeignKey(Place, on_delete=models.CASCADE, related_name='favorited_by')
+    added_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'place')  # prevents duplicate favorites
+
+    def __str__(self):
+        return f"{self.user.username} -> {self.place.name}"
