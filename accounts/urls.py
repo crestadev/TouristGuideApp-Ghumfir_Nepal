4da355d958f59bb23b56be1d4584f7bced3088c1
Dev <nameofdeev@gmail.com>
2025-10-26 21:50:50 +0545

    Ipdated accounts urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,8 +4,5 @@ from . import views
 urlpatterns = [
     path('signup/', views.signup_view, name='signup'),
     path('profile/', views.profile_view, name='profile'),
-
-    # Favorites / Wishlist URLs
-    path('favorites/add/<int:place_id>/', views.add_favorite_ajax, name='add_favorite_ajax'),
-    path('favorites/remove/<int:place_id>/', views.remove_favorite_ajax, name='remove_favorite_ajax'),
+    path('favorites/toggle/<int:place_id>/', views.toggle_favorite, name='toggle_favorite'),
 ]
