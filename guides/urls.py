25f7d57c86d87c89439a43678fe4e0da604ddcfa
Dev <nameofdeev@gmail.com>
2025-10-27 20:55:16 +0545

    added urls

--- guides/urls.py
+++ guides/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', views.place_list, name='place_list'),
     path('<slug:slug>/', views.place_detail, name='place_detail'),
     path('add/<int:place_id>/', views.add_to_itinerary, name='add_to_itinerary'),
+    path('places/favorite/<int:place_id>/', views.add_to_favorites, name='add_to_favorites'),
+    path('places/unfavorite/<int:place_id>/', views.remove_from_favorites, name='remove_from_favorites'),
+
 ]
