92cbeeaa5895a4ee7644dd84f5e43211157a9b49
Dev <nameofdeev@gmail.com>
2025-10-30 20:46:26 +0545

    Added url of remove itinerary

--- guides/urls.py
+++ guides/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     path('', views.place_list, name='place_list'),
     path('<slug:slug>/', views.place_detail, name='place_detail'),
     path('add/<int:place_id>/', views.add_to_itinerary, name='add_to_itinerary'),
+    path('remove-from-itinerary/<int:place_id>/', views.remove_from_itinerary, name='remove_from_itinerary'),
+
     path('places/favorite/<int:place_id>/', views.add_to_favorites, name='add_to_favorites'),
     path('places/unfavorite/<int:place_id>/', views.remove_from_favorites, name='remove_from_favorites'),   
 
